6acc3040aa213f4021da558c72cf5442099366df
Alexandre VANHOVE <vanhovea@pc5301-test.esiee.fr>
2018-01-09 16:11:47 +0100

    first message

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int main()
+{
+    char[] string = "jean-pierre";
+    dictionary[] = getSortedTab(string)
+}
+
+dictionary[] getSortedTab(string word)
+{
+    return 
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+typedef stuct{
+    char letter;
+    int numberOfOccurence;
+} dictionary
+
+dictionary[] getSortedTab(string word);
